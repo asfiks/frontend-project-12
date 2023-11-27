fcc28b0651dd7922aa093868a66ddf0f50ef0c66
asfiks <asfiks88@gmail.com>
2023-11-27 21:05:53 +0300

    test

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,10 +1,11 @@
 import React, {
-  createContext, useState, useEffect, useMemo,
+  createContext, useState, useEffect, useMemo, useCallback,
 } from 'react';
 import axios from 'axios';
 import api from '../routes/api';
 
 export const AuthContext = createContext();
+
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState();
   const [errorAuth, setErrorAuth] = useState();
@@ -16,27 +17,26 @@ export const AuthProvider = ({ children }) => {
     }
   }, []);
 
-  const login = async (user) => {
+  const login = useCallback(async (user) => {
     try {
       const response = await axios.post(api.loginPath(), user);
       const newToken = response.data.token;
       setToken(newToken);
       localStorage.setItem('token', newToken);
-
       localStorage.setItem('username', user.username);
     } catch (error) {
-      if (error.response.status === 401) {
+      if (error.response && error.response.status === 401) {
         setErrorAuth('401');
       }
       console.error('Ошибка при получении токена:', error);
     }
-  };
+  }, []);
 
-  const logout = () => {
+  const logout = useCallback(() => {
     localStorage.removeItem('token');
     localStorage.removeItem('username');
     setToken('');
-  };
+  }, []);
 
   const authContextValue = useMemo(() => ({
     token, errorAuth, login, logout,
